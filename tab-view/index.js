5a68f65e5ee6a39b7600fffc62667f72868a7ec7
Himanshu <himanshu@geekyants.com>
2018-03-19 12:10:58 +0530

    fixes

--- tab-view/index.js
+++ tab-view/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Text, View } from "react-native";
 import { TabViewAnimated, TabBar, SceneMap } from "react-native-tab-view";
+import uuid from "uuid/v4";
 
 class TabView extends React.Component {
   state = {
@@ -8,17 +9,52 @@ class TabView extends React.Component {
     routes: []
   };
 
-  componentDidMount() {
-    const routes = this.props.children.map(child => {
+  _updateNavState = nextProps => {
+    console.log("_updateNavState");
+    let props = this.props;
+    if (nextProps) {
+      props = nextProps;
+    }
+    const routes = props.children.map(child => {
       return {
-        key: child.props.title,
+        key: uuid(),
         title: child.props.title
       };
     });
 
+    console.log(routes, "wdwdwd", props.children);
+
     this.setState({
-      routes
+      routes: [...routes] // continue here, cannot update state in componentWillUpdate
     });
+  };
+
+  componentDidMount() {
+    this._updateNavState();
+  }
+
+  // componentWillUpdate(nextProps, nextState) {
+  //   console.log("componentWillUpdate", nextProps, nextState, this.props);
+  //   if (nextProps.children.length !== this.props.children.length) {
+  //     this._updateNavState(nextProps);
+  //   }
+  // }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log(
+      "componentDidUpdate",
+      prevProps,
+      prevState,
+      this.props,
+      this.state
+    );
+    if (
+      this.state.routes.length !== this.props.children.length ||
+      prevProps !== this.props
+    ) {
+      console.log("i am here");
+      this._updateNavState(this.props);
+    }
   }
 
   _handleIndexChange = index => {
@@ -39,15 +75,18 @@ class TabView extends React.Component {
 
     // console.log(this.props, this.state);
 
-    this.props.children.forEach(child => {
-      sceneMap[child.props.title] = () => child;
-    });
-
     if (this.state.routes.length === 0) {
       return null;
     }
 
-    // console.log(sceneMap, navigationState, "sceneMap, navigationState");
+    this.props.children.forEach((child, index) => {
+      console.log(this.state.routes, index, child, "testetsttest");
+      if (this.state.routes[index]) {
+        sceneMap[this.state.routes[index].key] = () => child;
+      }
+    });
+
+    console.log(sceneMap, navigationState, "sceneMap, navigationState");
 
     return (
       <TabViewAnimated
