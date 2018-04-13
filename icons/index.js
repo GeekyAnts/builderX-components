1cda6f0499eaa80b1d82ce235966baa078d996a3
Rishab Jain <rishab1065@gmail.com>
2018-04-13 12:10:36 +0530

    added @builderx/icons

---
+++ icons/index.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Entypo from "react-native-vector-icons/Entypo";
+import Feather from "react-native-vector-icons/Feather";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import Foundation from "react-native-vector-icons/Foundation";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import Octicons from "react-native-vector-icons/Octicons";
+import Zocial from "react-native-vector-icons/Zocial";
+import SimpleLineIcons from "react-native-vector-icons/SimpleLineIcons";
+import EvilIcons from "react-native-vector-icons/EvilIcons";
+
+class Icon extends React.Component {
+  setIcon(iconType) {
+    if (iconType === undefined) {
+      iconType = "MaterialCommunityIcons";
+    }
+    switch (iconType) {
+      case "Entypo":
+        this.Icon = Entypo;
+        break;
+      case "EvilIcons":
+        this.Icon = EvilIcons;
+        break;
+      case "Feather":
+        this.Icon = Feather;
+        break;
+      case "FontAwesome":
+        this.Icon = FontAwesome;
+        break;
+      case "Foundation":
+        this.Icon = Foundation;
+        break;
+      case "Ionicons":
+        this.Icon = Ionicons;
+        break;
+      case "MaterialCommunityIcons":
+        this.Icon = MaterialCommunityIcons;
+        break;
+      case "MaterialIcons":
+        this.Icon = MaterialIcons;
+        break;
+      case "Octicons":
+        this.Icon = Octicons;
+        break;
+      case "SimpleLineIcons":
+        this.Icon = SimpleLineIcons;
+        break;
+      case "Zocial":
+        this.Icon = Zocial;
+        break;
+      default:
+        this.Icon = Ionicons;
+    }
+  }
+
+  componentWillMount() {
+    this.setIcon(this.props.type);
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    if (nextProps.type && this.props.type != nextProps.type) {
+      this.setIcon(nextProps.type);
+    }
+  }
+
+  render() {
+    return <this.Icon ref={c => (this._root = c)} {...this.props} />;
+  }
+}
+export default Icon;
