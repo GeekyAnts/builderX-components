48711c0b0014d802c7eeee05b87d5e8e7856059c
Himanshu Satija <himanshu219s@gmail.com>
2020-06-23 20:52:09 +0530

    compile

---
+++ packages/utils/dist/index.js
@@ -0,0 +1,2 @@
+var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault");Object.defineProperty(exports,"__esModule",{value:true});Object.defineProperty(exports,"Center",{enumerable:true,get:function get(){return _CenterWrapper.default;}});var _CenterWrapper=_interopRequireDefault(require("./components/CenterWrapper"));
+//# sourceMappingURL=index.js.map
